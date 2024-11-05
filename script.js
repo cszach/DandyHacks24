662ab714a3a0167013164140b3eff724c903ad86
Zach <duong.nguyen@rochester.edu>
2024-11-05 01:25:29 -0500

    DandyHacks 2024!

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const wrapper = document.querySelector('.wrapper');
-    const navBar = document.querySelector('nav');
-    const navigationLinks = navBar.querySelectorAll('li');
-    const hamburgerCheckbox = navBar.querySelector('.peer');
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelector(".wrapper");
+  const navBar = document.querySelector("nav");
+  const navigationLinks = navBar.querySelectorAll("li");
+  const hamburgerCheckbox = navBar.querySelector(".peer");
 
-    // Hamburger navigation
+  // Hamburger navigation
 
-    navigationLinks.forEach(navigationLink => {
-        navigationLink.addEventListener('click', () => {
-            hamburgerCheckbox.checked = false;
-        });
+  navigationLinks.forEach((navigationLink) => {
+    navigationLink.addEventListener("click", () => {
+      hamburgerCheckbox.checked = false;
     });
+  });
 
-    // Navbar appears on scroll down and hides on scroll up
+  // Navbar appears on scroll down and hides on scroll up
 
-    let lastScrollY = wrapper.scrollTop;
+  let lastScrollY = document.documentElement.scrollTop;
 
-    wrapper.addEventListener('scroll', () => {
-        wrapper.scrollTop - lastScrollY > 0 ? hideNavBar() : showNavBar();
-        lastScrollY = wrapper.scrollTop;
-    });
+  window.addEventListener("scroll", () => {
+    document.documentElement.scrollTop - lastScrollY > 0
+      ? hideNavBar()
+      : showNavBar();
+    lastScrollY = document.documentElement.scrollTop;
+  });
 
-    function hideNavBar() {
-        navBar.classList.add('-translate-y-full');
-        navBar.classList.remove('translate-y-0');
-    }
+  function hideNavBar() {
+    navBar.classList.add("-translate-y-full");
+    navBar.classList.remove("translate-y-0");
+  }
 
-    function showNavBar() {
-        navBar.classList.add('translate-y-0');
-        navBar.classList.remove('-translate-y-full');
-    }
+  function showNavBar() {
+    navBar.classList.add("translate-y-0");
+    navBar.classList.remove("-translate-y-full");
+  }
 
-    // Close navigation menu when the dark overlay is clicked on
+  // Close navigation menu when the dark overlay is clicked on
 
-    const darkOverlay = navBar.querySelector('.bg-black');
+  const darkOverlay = navBar.querySelector(".bg-black");
 
-    darkOverlay.addEventListener('click', () => {
-        hamburgerCheckbox.checked = false;
-    });
-
-    // particles.js
-
-    particlesJS.load('particles-js', 'assets/particlesjs-config.json');
+  darkOverlay.addEventListener("click", () => {
+    hamburgerCheckbox.checked = false;
+  });
 });
